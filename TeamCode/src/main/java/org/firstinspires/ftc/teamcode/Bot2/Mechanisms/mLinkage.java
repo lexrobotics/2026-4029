369dfe23c25d10519d60f93e1497d64ad9e958fb
maggie zhu <107164272+magizee@users.noreply.github.com>
2025-03-06 16:27:46 -0500

    statestssates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot2/Mechanisms/mLinkage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot2/Mechanisms/mLinkage.java
@@ -6,7 +6,7 @@ import org.firstinspires.ftc.teamcode.Bot2.Mechanisms.AbstractMechanisms.ServoMe
 @Config
 public class mLinkage extends ServoMechanism {
     public mLinkage(){ super("Linkage"); }
-    public static double INIT = 0;
+    public static double INIT = 0.1;
     public static double REST = 0;
     public static double TRANSFER = 0;
     public static double TRANSFER_PREP = 0;
