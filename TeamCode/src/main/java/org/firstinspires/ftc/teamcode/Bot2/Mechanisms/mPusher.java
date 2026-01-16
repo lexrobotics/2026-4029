cea195010b71bd0521632ab4b10da4c44040f22a
IDoSomeStuff09 <sashasabr@gmail.com>
2026-01-16 09:09:27 -0800

    Uncommented this file and update the path to the hardwear map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot2/Mechanisms/mPusher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot2/Mechanisms/mPusher.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.Bot2.Mechanisms;
 import org.firstinspires.ftc.teamcode.Bot2.Mechanisms.AbstractMechanisms.ServoMechanism;
 
 public class mPusher extends ServoMechanism {
-    public mPusher() {super("Transfer");}
+    public mPusher() {super("Pusher");}
     public static double INIT = 0.055;
     public static double REST = 0.055;
     public static double PUSH = 0.128;
