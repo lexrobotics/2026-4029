a325db6e21c1d07d27151f3fd622c2056ec0e6f1
IDoSomeStuff09 <sashasabr@gmail.com>
2026-01-28 12:29:35 -0800

    Uncommented this file and update the path to the hardwear map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/opmode/TurnTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/opmode/TurnTest.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.roadrunner.drive.opmode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 
@@ -14,8 +16,11 @@ import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 public class TurnTest extends LinearOpMode {
     public static double ANGLE = 90; // deg
 
+    private ElapsedTime timer;
+
     @Override
     public void runOpMode() throws InterruptedException {
+        timer = new ElapsedTime();
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         waitForStart();
@@ -23,6 +28,26 @@ public class TurnTest extends LinearOpMode {
         if (isStopRequested()) return;
 
         drive.turn(Math.toRadians(ANGLE));
+
+        Pose2d poseEstimate = drive.getPoseEstimate();
+        telemetry.addData("finalHeading", poseEstimate.getHeading());
+        telemetry.addData("finalHeadingDeg", Math.toDegrees(poseEstimate.getHeading()));
+        telemetry.update();
+        timer.reset();
+        while (opModeIsActive() && timer.milliseconds() < 10000) {
+            sleep(5);
+        }
+
+//        drive.turn(Math.toRadians(-ANGLE));
+//
+//        Pose2d poseEstimate2 = drive.getPoseEstimate();
+//        telemetry.addData("finalHeading", poseEstimate2.getHeading());
+//        telemetry.addData("finalHeadingDeg", Math.toDegrees(poseEstimate2.getHeading()));
+//        telemetry.update();
+//        timer.reset();
+//        while (opModeIsActive() && timer.milliseconds() < 10000) {
+//            sleep(5);
+//        }
     }
 
 }
